b37ebf29b0eb6dee6d565054f03438f12370682a
elmashatri <94225968+elmashatri@users.noreply.github.com>
2022-04-10 00:27:34 +0200

    UDP Socket Server

---
+++ src/Server/main.java
@@ -0,0 +1,12 @@
+package Server;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class main {
+    public static  void main(String[] args) throws SocketException {
+        DatagramSocket datagramSocket = new DatagramSocket(1234);
+        Server server = new Server(datagramSocket);
+        server.receiveThenSend();
+    }
+}
