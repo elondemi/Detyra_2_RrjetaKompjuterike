c9d8fe47ae78ab94024024b716bde3f9da7b89e6
ell <elion.rrahmani@student.uni-pr.edu>
2022-04-10 01:56:10 +0200

    Implementimi i server side

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,2 +1,36 @@
+package Server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Server {
+    private DatagramSocket datagramSocket;
+    private byte[] buffer = new byte[256];
+
+    public Server(DatagramSocket datagramSocket) {
+        this.datagramSocket = datagramSocket;
+    }
+
+    public void receiveThenSend() {
+        while (true) {
+            try{
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+                datagramSocket.receive(datagramPacket);
+                InetAddress inetAddress = datagramPacket.getAddress();
+                int port = datagramPacket.getPort();
+                String messageFromClient = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+                System.out.println("Message from client: " + messageFromClient);
+                datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, port);
+                datagramSocket.send(datagramPacket);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+
+}
 
